1878868c1e151f27def974ba04cb6586542e4542
Khalid Rast <krast214@gmail.com>
2025-06-02 23:35:11 -0700

    Update server.mjs

--- config/plugins/robojs/server.mjs
+++ config/plugins/robojs/server.mjs
@@ -1,3 +1,9 @@
 export default {
 	cors: true
 }
+const port = process.env.PORT || 8080;
+const host = process.env.HOST || '0.0.0.0';
+
+app.listen(port, host, () => {
+    console.log(`Server running on ${host}:${port}`);
+});
